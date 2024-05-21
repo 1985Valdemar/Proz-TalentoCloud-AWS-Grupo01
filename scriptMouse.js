7a9c8ef83136d0ba85d43215950cb4328c0bf70c
1985Valdemar <vteider@yahoo.com>
2024-05-21 10:12:24 -0300

    Atualizando Main para ir para Valdemar

--- scriptMouse.js
+++ scriptMouse.js
@@ -91,3 +91,64 @@ function formatarNumero(numero) {
       botao.style.backgroundColor = "";
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Selecionando todos os elementos com a classe "botao-texto"
+    var botaoTextos = document.querySelectorAll(".botao-texto a, .botoes-home a, .btn-adicionar a");
+
+    // Iterando sobre cada elemento com a classe "botao-texto"
+    botaoTextos.forEach(function(botaoTexto) {
+        // Removendo o sublinhado e definindo a cor da letra para vermelho
+        botaoTexto.style.textDecoration = "none"; // Remove o sublinhado
+        botaoTexto.style.color = "white"; // Define a cor da letra para vermelho
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecionando todos os elementos com a classe "botao-texto"
+    var botaoTextos = document.querySelectorAll(".btn-item a");
+
+    // Iterando sobre cada elemento com a classe "botao-texto"
+    botaoTextos.forEach(function(botaoTexto) {
+        // Removendo o sublinhado e definindo a cor da letra para vermelho
+        botaoTexto.style.textDecoration = "none"; // Remove o sublinhado
+        botaoTexto.style.color = "white"; // Define a cor da letra para vermelho
+    });
+});
+
+// Selecionando o botão
+var botao = document.querySelector(".btn-item");
+
+// Verificando se o botão existe antes de adicionar ouvintes de eventos
+if (botao) {
+    // Adicionando um ouvinte de evento para o evento de passagem do cursor (mouseover)
+    botao.addEventListener("mouseover", function() {
+        // Muda a cor de fundo do botão quando o cursor passa por cima
+        botao.style.backgroundColor = "#00FFFF";
+    });
+
+    // Adicionando um ouvinte de evento para o evento de retirada do cursor (mouseout) do botão
+    botao.addEventListener("mouseout", function() {
+        // Restaura a cor original do botão
+        botao.style.backgroundColor = "";
+    });
+}
+
+// Selecionando todos os botões da página
+var botoes = document.querySelectorAll(".botoes-home, .btn-adicionar, .remover-carrinho1, .btn-item");
+
+// Iterando sobre cada botão
+botoes.forEach(function(botao) {
+    // Adicionando um ouvinte de evento para o evento de passagem do cursor (mouseover)
+    botao.addEventListener("mouseover", function() {
+        // Muda a cor de fundo do botão quando o cursor passa por cima
+        botao.style.backgroundColor = "#fdb19a";
+    });
+
+    // Adicionando um ouvinte de evento para o evento de retirada do cursor (mouseout) do botão
+    botao.addEventListener("mouseout", function() {
+        // Restaura a cor original do botão
+        botao.style.backgroundColor = "";
+    });
+});
